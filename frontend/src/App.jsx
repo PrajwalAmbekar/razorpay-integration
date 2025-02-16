e959ba4af6d20c3c5910a29ddd6aafa5c802724d
Prajwal <ambekarprajwal33@gmail.com>
2025-02-16 10:04:59 +0530

    deleted old code of app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,9 @@
 import React from 'react'
 
 const App = () => {
-  const amount=500;
-  const currency="INR";
-  const receiptId="12345";
-  const paymenthandler=async (e)=>{
-    // alert("the payment is proceed")
-    const  response=await fetch("http://localhost:5000/order",{
-      method:"POST",
-      headers:{
-        "Content-type":"application/json"
-      },
-      body:JSON.stringify({
-        amount,
-        currency,
-        receipt:receiptId
-      })
-    })
-    const order=await response.json();
-    console.log("order",order);
-  }
   return (
     <div>
-      <div className='product'>
-        <h1>Razorpay payment gateway</h1>
-        <button className='button' onClick={paymenthandler}>Pay now</button>
-
-      </div>
+      
     </div>
   )
 }
